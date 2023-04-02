48533fd63782730279743c294d19f23b6039bf93
jsztutor <99634691+jsztutor@users.noreply.github.com>
2023-04-02 15:46:58 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   mode: "jit",
-  purge: ["./index.html", "./src/**/*.js"],
+  // purge: ["./index.html", "./src/**/*.js"], // this command is obselete
+   content: ["./index.html", "./src/**/*.js"],
+  // content: [
+  //   "./pages/**/*.{js,ts,jsx,tsx}",
+  //   "./components/**/*.{js,ts,jsx,tsx}",   // newer version i implement here
+  // ],
  
   theme: {
     extend: {},
